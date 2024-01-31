4c258f4509442887af3f67bd5fcdc2df8628ae38
Alester-Davis <alesterkvp@gmail.com>
2024-01-31 17:17:17 +0530

    found location

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,29 @@
+import { useEffect, useState } from 'react'
 import './App.css'
 
 function App() {
+  const [longitide,setLongitude] = useState(null)
+  const [latitude,setLatitude] = useState(null)
+  const [error,setError] = useState(null)
+  useEffect(()=>{
+    const successHandler = (position)=>{
+      setLongitude(position.coords.longitude)
+      setLatitude(position.coords.latitude)
+    }
+    const errorHandler = (error)=>{
+      setError(error)
+    }
+    if(!navigator.geolocation){
+      setError("Error")
+    }
+    navigator.geolocation.getCurrentPosition(successHandler,errorHandler)
+  },[])
   return (
     <>
       <h1>Alester</h1>
+      <h1>{longitide}</h1>
+      <h1>{"latitide"+latitude}</h1>
+      <h1>{error}</h1>
     </>
   )
 }
